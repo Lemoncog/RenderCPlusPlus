cc7a2c0f182982b15318fb83babcbcedb9215b28
Jason Leeming <Jason.Leeming@bbc.co.uk>
2017-05-17 16:52:41 +0100

    Using project from JamesShepd, but wrote CMake file to copy resources into build folder

--- main.cpp
+++ main.cpp
@@ -1,43 +1,27 @@
+#include <iostream>
+#include "geometry.h"
 #include "tgaimage.h"
-#include <math.h>
+#include "model.h"
 
-const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
-
-void box(TGAImage &image, int xStart, int yStart, int xEnd, int yEnd);
-void line(TGAImage &image, int xStart, int yStart, int xEnd, int yEnd);
-
-int main(int argc, char** argv) {
-	TGAImage image(100, 100, TGAImage::RGB);
-
-    line(image, 0, 0, 85, 25);
-
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga");
-	return 0;
-}
-
-void line(TGAImage &image, int xStart, int yStart, int xEnd, int yEnd) {
-    int xDiff = abs(xStart - xEnd);
-    int yDiff = abs(yStart - yEnd);
-
-    float lineLength = sqrt((xDiff*xDiff)+(yDiff*yDiff));
-    float xStep = xDiff/lineLength;
-    float yStep = yDiff/lineLength;
-
-    for(int s = 0; s < lineLength; s++) {
-        image.set(xStart+(xStep*s), yStart+(yStep*s), white);
-    }
-}
-
-void box(TGAImage &image, int xStart, int yStart, int xEnd, int yEnd) {
-    int xDiff = abs(xStart - xEnd);
-    int yDiff = abs(yStart - yEnd);
-
-    for(int x = 0; x < xDiff; x++) {
-        for(int y = 0; y < yDiff; y++) {
-            image.set(xStart+x, yStart+y, white);
+int main(int argc, const char * argv[]) {
+    
+    Model model("obj/african_head/african_head.obj");
+    
+    std::cout << "loaded model, " << model.nfaces() << " faces\n";
+    
+    Vec3f vec(1.0f, 0.0f, 0.0f);
+    
+    TGAImage image(256, 256, 4);
+    
+    for(int x=0; x<image.get_width(); x++) {
+        for (int y=0; y<image.get_height(); y++) {
+            image.set(x, y, TGAColor(x, y, 0));
         }
     }
+    
+    bool done = image.write_tga_file("image.tga");
+    
+    std::cout << "Hello, World!\n" << vec << "\n" << ( done ? "done" : "failed" ) << "\n";
+    
+    return 0;
 }
-
