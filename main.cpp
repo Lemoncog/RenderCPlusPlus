f4f9622be8d0dac6fe0691c8465fc4d702db5fa4
Jason Leeming <Jason.Leeming@bbc.co.uk>
2017-05-19 11:04:18 +0100

    Moving towards line algroithm from wiki

--- main.cpp
+++ main.cpp
@@ -7,9 +7,12 @@
 void box(TGAImage &image, int xStart, int yStart, int xEnd, int yEnd);
 void line(TGAImage &image, Vec2f start, Vec2f end);
 void line(TGAImage &image, Vec2f start, Vec2f end, TGAColor color);
+void lineB(TGAImage &image, Vec2f start, Vec2f end, TGAColor color);
+void lineA(TGAImage &image, Vec2f start, Vec2f end, TGAColor color);
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor blue  = TGAColor(0, 255, 255, 255);
 
 int main(int argc, const char * argv[]) {
     
@@ -22,18 +25,19 @@ int main(int argc, const char * argv[]) {
     float width = 100;
     float height = 100;
     TGAImage image(width, height, TGAImage::RGB);
-    
+
     line(image, Vec2f(width, height), Vec2f(0.f, 0.f), white);
     line(image, Vec2f(10.f, 0), Vec2f(width, 95.f), red);
+    line(image, Vec2f(35.f, 35), Vec2f(70.f, 25.f), white);
 
-    line(image, Vec2f(0.f, height), Vec2f(width, 0.f), white);
-    line(image, Vec2f(width, 0), Vec2f(0.f, 95.f), red);
-
-    line(image, Vec2f(50.f, height), Vec2f(50.f, 0.f), white);
-    line(image, Vec2f(55.f, 0), Vec2f(55.f, height), red);
-
-    line(image, Vec2f(0.f, 50.f), Vec2f(width, 50.f), white);
-    line(image, Vec2f(width, 55.f), Vec2f(0.f, 55.f), red);
+//    line(image, Vec2f(0.f, height), Vec2f(width, 0.f), white);
+//    line(image, Vec2f(width, 0), Vec2f(0.f, 95.f), red);
+//
+//    line(image, Vec2f(50.f, height), Vec2f(50.f, 0.f), white);
+//    line(image, Vec2f(55.f, 0), Vec2f(55.f, height), red);
+//
+//    line(image, Vec2f(0.f, 50.f), Vec2f(width, 50.f), white);
+//    line(image, Vec2f(width, 55.f), Vec2f(0.f, 55.f), red);
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
@@ -43,18 +47,41 @@ int main(int argc, const char * argv[]) {
     
     return 0;
 }
-
 void line(TGAImage &image, Vec2f start, Vec2f end, TGAColor color) {
-    int xDiff = end.x- start.x;
+    lineA(image, start, end, color);
+}
+
+void lineA(TGAImage &image, Vec2f start, Vec2f end, TGAColor color) {
+    float startX = start.x;
+    float startY = start.y;
+
+    float endX = end.x;
+    float endY = end.y;
+
+    for (float t=0.; t<1.; t+=.01) {
+
+        int x = startX * (1. - t) + endX * t;
+        int y = startY*(1.-t) + endY*t;
+        image.set(x, y, color);
+    }
+}
+
+void lineB(TGAImage &image, Vec2f start, Vec2f end, TGAColor color) {
+    int xDiff = end.x - start.x;
     int yDiff = end.y - start.y;
 
     float lineLength = abs(sqrt((xDiff*xDiff)+(yDiff*yDiff)));
+
     float xStep = xDiff/lineLength;
+
     float yStep = yDiff/lineLength;
 
     for(int s = 0; s < lineLength; s++) {
         image.set(start.x+(xStep*s), start.y+(yStep*s), color);
     }
+
+    image.set(start.x, start.y, blue);
+    image.set(end.x, end.y, blue);
 }
 
 void line(TGAImage &image, Vec2f start, Vec2f end) {
